df7498f09e5d2326f10eb82d688126624774a329
Kristian Clifford <kricliff@gmail.com>
2026-09-02 12:21:08 +0200

    Stor forbokstav pa hvert ord i rolle/kundenavn, ikke bare det forste

--- app.js
+++ app.js
@@ -1109,10 +1109,14 @@ function byggKort(o) {
 }
 
 // "små bokstaver med stor forbokstav" - Recman-titler/kundenavn kommer ofte i store
-// bokstaver eller inkonsekvent skriving, normaliseres til vanlig setningsformatering.
+// bokstaver eller inkonsekvent skriving, normaliseres til vanlig formatering. Består
+// teksten av flere ord, får hvert av dem stor forbokstav (ikke bare det aller første).
 function storForbokstav(tekst) {
-  const lav = tekst.toLowerCase();
-  return lav.charAt(0).toUpperCase() + lav.slice(1);
+  return tekst
+    .toLowerCase()
+    .split(" ")
+    .map((ord) => ord.charAt(0).toUpperCase() + ord.slice(1))
+    .join(" ");
 }
 
 function kortHoyreTekst(o) {
